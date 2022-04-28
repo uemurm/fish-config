a35d51b3a820916bb61bb6c493b63ed7a6ef1fc8
Mitsu Uemura <mitsuhide.uemura@gmail.com>
2022-04-29 01:09:01 +1000

    Minimum configurations such as a prompt.

---
+++ config.fish
@@ -0,0 +1,17 @@
+# View
+set -g theme_display_date yes
+set -g theme_date_format "+%F %H:%M"
+set -g theme_display_git_default_branch yes
+
+# Abbreviate
+abbr -a -U -- cp 'cp -i'
+abbr -a -U -- la 'ls -hla'
+abbr -a -U -- ll 'ls -hl'
+abbr -a -U -- lld 'ls -hld'
+abbr -a -U -- llt 'ls -hltr'
+abbr -a -U -- llta 'ls -hltra'
+abbr -a -U -- lt 'ls -tr'
+abbr -a -U -- mv 'mv -i'
+abbr -a -U -- rm 'rm -i'
+
+set -x LESS '--ignore-case --LONG-PROMPT'
