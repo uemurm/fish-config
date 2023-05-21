c3022ac3d38dade48898fed8a4a5d406d4e7c252
Mitsu Uemura <mitsuhide.uemura@gmail.com>
2023-05-21 16:21:03 +1000

    Fix VIDUAL variable spelling. Add alias of lla

--- config.fish
+++ config.fish
@@ -5,14 +5,15 @@ set -g theme_display_git_default_branch yes
 
 # Abbreviate
 abbr -a -U -- cp 'cp -i'
-abbr -a -U -- la 'ls -hla'
-abbr -a -U -- ll 'ls -hl'
-abbr -a -U -- lld 'ls -hld'
-abbr -a -U -- llt 'ls -hltr'
+abbr -a -U -- la   'ls -ha'
+abbr -a -U -- ll   'ls -hl'
+abbr -a -U -- lla  'ls -hla'
+abbr -a -U -- lld  'ls -hld'
+abbr -a -U -- llt  'ls -hltr'
 abbr -a -U -- llta 'ls -hltra'
-abbr -a -U -- lt 'ls -tr'
+abbr -a -U -- lt   'ls -tr'
 abbr -a -U -- mv 'mv -i'
 abbr -a -U -- rm 'rm -i'
 
 set -x LESS '--ignore-case --LONG-PROMPT'
-set -x VIDUAL 'vim'
+set -x VISUAL 'vim'
