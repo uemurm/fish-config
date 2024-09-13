d6df9586a36e18a54cad403ab4ea773a66f5e2cd
Mitsu Uemura <mitsuhide.uemura@morsemicro.com>
2024-09-13 19:12:24 +1000

    Modify abbr for git commit.

--- config.fish
+++ config.fish
@@ -25,7 +25,7 @@ abbr --add -- gst	'git status'
 abbr --add -- gdiff	'git diff'
 abbr --add -- gbr	'git branch'
 abbr --add -- gadd	'git add'
-abbr --add -- gci	'git commit'
+abbr --add -- gci	'git commit -m'
 abbr --add -- gco	'git checkout'
 abbr --add -- gps	'git push'
 abbr --add -- gpl	'git pull'
@@ -42,9 +42,9 @@ abbr --add -- drmi		'docker rmi'
 abbr --add -- dstop		'docker stop'
 abbr --add -- dpull		'docker pull'
 
-set --export LESS '--ignore-case --LONG-PROMPT --RAW-CONTROL-CHARS'
-set --export LESSOPEN '| /usr/share/source-highlight/src-hilite-lesspipe.sh %s'
-set --export VISUAL 'vi'
+set --export LESS	'--ignore-case --LONG-PROMPT --RAW-CONTROL-CHARS'
+set --export LESSOPEN	'| /usr/share/source-highlight/src-hilite-lesspipe.sh %s'
+set --export VISUAL	'vi'
 
 # Run `ssh-agent` on start-up in order to omit passphrase input.
 # But turned out that this variable is not set even though a process is already running.
