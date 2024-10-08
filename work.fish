014fae3122f074fcb0f5572bfc4d470ca78532c6
Mitsu Uemura <mitsuhide.uemura@gmail.com>
2024-10-08 16:18:27 +1100

    Access SVT production DB.

---
+++ work.fish
@@ -0,0 +1,4 @@
+# Credentials to access SVT production DB
+set --export SVT_USR "svt_prod_read_only"
+set --export SVT_PWD "99jY7#zXTtrt^uW"
+set --export SVT_DB "morse-data.che3yld3w1kr.ap-southeast-2.rds.amazonaws.com/svt"
