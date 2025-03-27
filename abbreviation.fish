b74e444d7705ee43d249d9e405c1dda73faa37d3
Mitsu Uemura <mitsuhide.uemura@gmail.com>
2025-03-27 13:21:34 +1100

    Refactor command abbreviations into a separate file for better organization

---
+++ abbreviation.fish
@@ -0,0 +1,45 @@
+## General commands
+abbr --add -- ls    'ls -F'
+abbr --add -- la    'ls -hFa'
+abbr --add -- ll    'ls -hFl'
+abbr --add -- lla   'ls -hFla'
+abbr --add -- lld   'ls -hFld'
+abbr --add -- llt   'ls -hFltr'
+abbr --add -- llta  'ls -hFltra'
+abbr --add -- lt    'ls -Ftr'
+
+abbr --add -- cp    'cp -i'
+abbr --add -- mv    'mv -i'
+abbr --add -- rm    'rm -i'
+abbr --add -- p     'pushd'
+abbr --add -- d	    'dirs'
+
+abbr --add -- vi    'vim'
+
+#
+# Git related
+#
+abbr --add -- gst   'git status'
+abbr --add -- gdiff 'git diff'
+abbr --add -- gbr   'git branch'
+abbr --add -- gadd  'git add'
+abbr --add -- gci   'git commit -m'
+abbr --add -- gco   'git checkout'
+abbr --add -- gps   'git push'
+abbr --add -- gpl   'git pull'
+abbr --add -- gf    'git fetch'
+abbr --add -- glog  'git log'
+abbr --add -- gshow 'git show'
+
+#
+# Docker related
+#
+abbr --add -- dps       'docker ps --all'
+abbr --add -- dimages   'docker images --all'
+abbr --add -- dhistory  'docker history'
+abbr --add -- dexec     'docker exec'
+abbr --add -- dlogs     'docker logs'
+abbr --add -- drm       'docker rm'
+abbr --add -- drmi      'docker rmi'
+abbr --add -- dstop     'docker stop'
+abbr --add -- dpull     'docker pull'
--- config.fish
+++ config.fish
@@ -3,53 +3,6 @@ set --global theme_display_date yes
 set --global theme_date_format "+%F %H:%M"
 set --global theme_display_git_default_branch yes
 
-# Abbreviate
-abbr --add -- vi    'vim'
-
-## General commands
-abbr --add -- ls    'ls -F'
-abbr --add -- la    'ls -hFa'
-abbr --add -- ll    'ls -hFl'
-abbr --add -- lla   'ls -hFla'
-abbr --add -- lld   'ls -hFld'
-abbr --add -- llt   'ls -hFltr'
-abbr --add -- llta  'ls -hFltra'
-abbr --add -- lt    'ls -Ftr'
-
-abbr --add -- cp    'cp -i'
-abbr --add -- mv    'mv -i'
-abbr --add -- rm    'rm -i'
-abbr --add -- p     'pushd'
-abbr --add -- d	    'dirs'
-
-#
-# Git related
-#
-abbr --add -- gst   'git status'
-abbr --add -- gdiff 'git diff'
-abbr --add -- gbr   'git branch'
-abbr --add -- gadd  'git add'
-abbr --add -- gci   'git commit -m'
-abbr --add -- gco   'git checkout'
-abbr --add -- gps   'git push'
-abbr --add -- gpl   'git pull'
-abbr --add -- gf    'git fetch'
-abbr --add -- glog  'git log'
-abbr --add -- gshow 'git show'
-
-#
-# Docker related
-#
-abbr --add -- dps       'docker ps --all'
-abbr --add -- dimages   'docker images --all'
-abbr --add -- dhistory  'docker history'
-abbr --add -- dexec     'docker exec'
-abbr --add -- dlogs     'docker logs'
-abbr --add -- drm       'docker rm'
-abbr --add -- drmi      'docker rmi'
-abbr --add -- dstop     'docker stop'
-abbr --add -- dpull     'docker pull'
-
 set --export CDPATH     $HOME/.config/
 set --export LESS       '--ignore-case --LONG-PROMPT --RAW-CONTROL-CHARS'
 set --export LESSOPEN   '| /usr/share/source-highlight/src-hilite-lesspipe.sh %s'
@@ -72,6 +25,11 @@ pyenv init - | source  # Load pyenv automatically.
 
 # Add poetry and other commands to path
 set --export PATH   "$HOME/.local/bin:$PATH"
+
+set ABBREVIATIONS $HOME/.config/fish/abbreviation.fish
+if test -e "$ABBREVIATIONS"
+    source $ABBREVIATIONS
+end
 
 # Work-related config
 set WORK_CONFIG $HOME/.config/fish/work.fish
