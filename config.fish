b9335107ae396d01196364081980bd68bc8011c0
Mitsu Uemura <mitsuhide.uemura@gmail.com>
2025-11-29 10:58:54 +0900

    VISUAL is checked and used first when opening an editor from less

--- config.fish
+++ config.fish
@@ -3,19 +3,18 @@ set --global theme_display_date yes
 set --global theme_date_format "+%F %H:%M"
 set --global theme_display_git_default_branch yes
 
-set --export CDPATH     $HOME/.config/
 set --export LESS       '--ignore-case --LONG-PROMPT --RAW-CONTROL-CHARS'
-
 switch (uname --kernel-name)
 case Linux
     set --export LESSOPEN   '| /usr/share/source-highlight/src-hilite-lesspipe.sh %s'	# Mainly for WSL
 case 'CYGWIN*' 'MINGW*' 'MSYS*'
     set --export LESSOPEN   '| /mingw64/bin/highlight --out-format=ansi %s'
 case '*'
-    echo "Unknown OS: "(uname -s)
+    echo "Unknown OS: "(uname --kernel-name)
 end
 
-set --export VISUAL     'vi'
+set --export VISUAL     'vim'
+set --export CDPATH     $HOME/.config/
 
 set --universal fish_greeting   # Unset greeting from fish shell.
 
